856f1d71057c7e783109dddf74268371070f859b
Alex <alex.kleinman@gmail.com>
2024-05-16 15:15:06 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(record) {
+  for (const object of record) {
+    if (object.result === "W") {
+      return object.year;
+    }
+  }
+}
+
+superbowlWin(record);
